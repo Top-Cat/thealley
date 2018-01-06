b3d1d2ede64d91c8e0ddf3cd8ab2603a27d6ee87
Thomas Cheyney <thomas.cheyney@skybettingandgaming.com>
2018-01-06 20:14:28 +0000

    Add icons to ui

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,12 +1,15 @@
-Light = function(id, hostname) {
+Light = function(id, name, hostname) {
     this.hostname = hostname;
+    this.name = name;
     this.id = id;
     this.state = false;
     this.obj = this.gen();
 };
 Light.prototype.gen = function() {
-    return $("<div />", {text: 'Light: ' + this.hostname})
-        .click($.proxy(this.toggle, this));
+    return $("<div />", {class: 'far'})
+        .click($.proxy(this.toggle, this))
+        .append($("<span />", {html: '&#xf0eb;'}))
+        .append(this.name);
 };
 Light.prototype.toggle = function() {
     that = this;
@@ -20,7 +23,7 @@ Light.prototype.toggle = function() {
 };
 Light.prototype.updateState = function(state) {
     this.state = state;
-    this.obj.css('color', state ? 'red' : 'blue');
+    this.obj.css('color', state ? '#5d5' : '#888');
 };
 Light.prototype.render = function() {
     $('body').append(this.obj);
