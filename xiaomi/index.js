8960fa038f18afcccef8af05ea189a8153f91ff7
Thomas Cheyney <thomas.cheyney@skybettingandgaming.com>
2018-06-18 14:54:11 +0100

    Fix multiline logging

--- xiaomi/index.js
+++ xiaomi/index.js
@@ -9,6 +9,10 @@ miio.device({ token: '2e95785a0b8a5c1a5cbcf07accbb17f0', address: '10.48.8.86' }
 	.then(onConnected)
 	.catch(err => console.log(err));
 
+function log(str) {
+	console.log(JSON.stringify(str));
+}
+
 function onConnected(dev) {
 	const children = dev.children();
 	for (const child of children) {
@@ -19,7 +23,7 @@ function onConnected(dev) {
 }
 
 function valueEvent(ctx, prop, v) {
-	console.log({event: prop, id: ctx.id, value: v});
+	log({event: prop, id: ctx.id, value: v});
 
 	request.post({uri: process.env.BASE_URL + "/prop", json: {property: prop, value: v.value, sensor: ctx.id}, timeout: 2000}, function (error, response, body) {
 		//console.log(JSON.stringify([String(error), String(response), String(body)]));
@@ -27,7 +31,7 @@ function valueEvent(ctx, prop, v) {
 }
 
 function motionEvent(ctx) {
-	console.log({event: "movement", id: ctx.id});
+	log({event: "movement", id: ctx.id});
 
 	request.post({uri: process.env.BASE_URL + "/motion", json: {sensor: ctx.id}, timeout: 2000}, function (error, response, body) {
 		//console.log(JSON.stringify([String(error), String(response), String(body)]));
