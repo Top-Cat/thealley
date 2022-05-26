c643f1457929846503ded8f273d6a86b752be32c
Thomas Cheyney <top_cat@thomasc.co.uk>
2022-05-26 18:29:46 +0100

    Lint

--- src/main/kotlin/uk/co/thomasc/thealley/repo/SwitchRepository.kt
+++ src/main/kotlin/uk/co/thomasc/thealley/repo/SwitchRepository.kt
@@ -1,6 +1,5 @@
 package uk.co.thomasc.thealley.repo
 
-import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
