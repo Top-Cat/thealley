8f723460196f71234168b41b0921dbedd25bc541
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-01-22 17:21:02 +0000

    Remove other transient

--- src/jvmMain/kotlin/uk/co/thomasc/thealley/devices/alarm/TexecomZone.kt
+++ src/jvmMain/kotlin/uk/co/thomasc/thealley/devices/alarm/TexecomZone.kt
@@ -1,12 +1,10 @@
 package uk.co.thomasc.thealley.devices.alarm
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 
 @Serializable
 data class TexecomZone(
     val name: String,
-    @Transient
     val slug: String? = null,
     val number: Int,
     val status: ZoneState,
