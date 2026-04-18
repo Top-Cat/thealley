05ca57669aff9394eeb7d58ac7b716b6a6577e7e
Thomas Cheyney <top_cat@thomasc.co.uk>
2026-04-18 18:25:45 +0100

    Allow scene switch to control multiple devices

--- src/commonMain/kotlin/uk/co/thomasc/thealley/devices/types/SceneSwitchConfig.kt
+++ src/commonMain/kotlin/uk/co/thomasc/thealley/devices/types/SceneSwitchConfig.kt
@@ -13,7 +13,9 @@ data class SceneSwitchConfig(
     override val name: String,
     override val deviceId: String,
     override val prefix: String = "zigbee",
-    val targets: List<Int>
+    val targets0: List<Int>,
+    val targets1: List<Int>,
+    val targets2: List<Int>
 ) : IAlleyConfig<SceneSwitchState>,
     IZigbeeConfig<SceneSwitchState>,
     IConfigEditable<SceneSwitchConfig> by SimpleConfigEditable(
@@ -21,12 +23,11 @@ data class SceneSwitchConfig(
             SceneSwitchConfig::name.fieldEditor("Name") { c, n -> c.copy(name = n) },
             SceneSwitchConfig::deviceId.fieldEditor("Device ID") { c, n -> c.copy(deviceId = n) },
             SceneSwitchConfig::prefix.fieldEditor("MQTT Prefix") { c, n -> c.copy(prefix = n) },
-            SceneSwitchConfig::targets.fieldEditor("Targets") { c, n -> c.copy(targets = n) }
+            SceneSwitchConfig::targets0.fieldEditor("Target Button 0") { c, n -> c.copy(targets0 = n) },
+            SceneSwitchConfig::targets1.fieldEditor("Target Button 1") { c, n -> c.copy(targets1 = n) },
+            SceneSwitchConfig::targets2.fieldEditor("Target Button 2") { c, n -> c.copy(targets2 = n) }
         )
     ) {
-    fun shar() {
-        TODO("Not yet implemented")
-    }
 
     override val defaultState = SceneSwitchState()
     override val stateSerializer = SceneSwitchState.serializer()
--- src/jvmMain/kotlin/uk/co/thomasc/thealley/devices/zigbee/moes/SceneSwitchDevice.kt
+++ src/jvmMain/kotlin/uk/co/thomasc/thealley/devices/zigbee/moes/SceneSwitchDevice.kt
@@ -12,22 +12,31 @@ import uk.co.thomasc.thealley.devices.zigbee.custom.ZBlindDevice
 class SceneSwitchDevice(id: Int, config: SceneSwitchConfig, state: SceneSwitchState, stateStore: IStateUpdater<SceneSwitchState>, val dev: AlleyDeviceMapper) :
     ZigbeeDevice<SceneSwitchUpdate, SceneSwitchDevice, SceneSwitchConfig, SceneSwitchState>(id, config, state, stateStore, SceneSwitchUpdate.serializer()) {
 
+    val targets = listOf(
+        config.targets0,
+        config.targets1,
+        config.targets2
+    )
+
     override suspend fun onUpdate(bus: AlleyEventEmitter, update: SceneSwitchUpdate) {
         update.action?.let { action ->
             updateState(state.copy(state = state.state.plus(action.button to !state.state.getOrDefault(action.button, false))))
 
-            config.targets.getOrNull(action.button - 1)?.let { targetDevice ->
-                val device = dev.getDevice(targetDevice)
+            targets.getOrNull(action.button - 1)?.let { targetIds ->
+                val devices = targetIds.map { dev.getDevice(it) }
 
-                if (device is ZBlindDevice) {
-                    val cmd = when {
-                        action.times > 1 -> BlindCommand.STOP
-                        action.button == 1 -> BlindCommand.OPEN
-                        else -> BlindCommand.CLOSE
+                devices.forEach { device ->
+                    if (device is ZBlindDevice) {
+                        val cmd = when {
+                            action.times > 1 -> BlindCommand.STOP
+                            action.button == 1 -> BlindCommand.OPEN
+                            else -> BlindCommand.CLOSE
+                        }
+                        device.sendCommand(bus, cmd)
                     }
-                    device.sendCommand(bus, cmd)
+
+                    // TODO: Other device types
                 }
-                // TODO: Other device types
             }
         }
     }
