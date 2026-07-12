cd039601a52f621b0935b176ccd73f1e3f467478
Thomas Cheyney <top_cat@thomasc.co.uk>
2026-07-12 12:13:13 +0100

    Env to configure mdns iface

--- src/jvmMain/kotlin/uk/co/thomasc/thealley/LocalDiscovery.kt
+++ src/jvmMain/kotlin/uk/co/thomasc/thealley/LocalDiscovery.kt
@@ -1,21 +1,24 @@
 package uk.co.thomasc.thealley
 
 import java.net.Inet4Address
+import java.net.InetAddress
 import java.net.NetworkInterface
 import javax.jmdns.JmDNS
 import javax.jmdns.ServiceInfo
 
+val discoveryInterface = System.getenv("DISCOVERY_IFACE") ?: "net1"
+
 class LocalDiscovery {
     fun start() {
-        val lanIp = NetworkInterface.getByName("net1")
+        val address = NetworkInterface.getByName(discoveryInterface)
             .inetAddresses.asSequence()
             .filterIsInstance<Inet4Address>()
             .first()
 
-        val jmdns = JmDNS.create(lanIp)
+        val jmdns = JmDNS.create(address)
         val discoveryData = mapOf(
             "id" to "thealley",
-            "ip" to "10.2.238.149",
+            "ip" to InetAddress.getLocalHost().hostAddress,
             "port" to "8080"
         )
         val serviceInfo = ServiceInfo.create("_alley._tcp.local.", "hub", 8080, 1, 1, false, discoveryData)
