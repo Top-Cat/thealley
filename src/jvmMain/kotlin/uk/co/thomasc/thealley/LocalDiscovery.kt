b4eb9872572cc33e0088089c20e06cb076ef1ac6
Thomas Cheyney <top_cat@thomasc.co.uk>
2026-07-12 10:06:11 +0100

    Refactor mDNS

--- src/jvmMain/kotlin/uk/co/thomasc/thealley/LocalDiscovery.kt
+++ src/jvmMain/kotlin/uk/co/thomasc/thealley/LocalDiscovery.kt
@@ -1,15 +1,24 @@
 package uk.co.thomasc.thealley
 
+import java.net.Inet4Address
 import java.net.InetAddress
+import java.net.NetworkInterface
 import javax.jmdns.JmDNS
 import javax.jmdns.ServiceInfo
 
 class LocalDiscovery {
     fun start() {
-        val jmdns = JmDNS.create(InetAddress.getByName("10.2.1.2"))
+        val lanIp = NetworkInterface.getByName("net1")
+            .inetAddresses.asSequence()
+            .filterIsInstance<Inet4Address>()
+            .first()
+
+        val calicoIp = InetAddress.getLocalHost().hostAddress
+
+        val jmdns = JmDNS.create(lanIp)
         val discoveryData = mapOf(
             "id" to "thealley",
-            "ip" to InetAddress.getLocalHost().hostAddress,
+            "ip" to calicoIp,
             "port" to "8080"
         )
         val serviceInfo = ServiceInfo.create("_alley._tcp.local.", "hub", 8080, 1, 1, false, discoveryData)
